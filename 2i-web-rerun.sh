6dcd4f776c45666e2bff4f2bfb2af8bc23c5c8a3
Gerhard Poul <gerhard.poul@gmail.com>
2018-06-14 13:15:28 +0200

    Script automatic deployment initiation

--- 2i-web-rerun.sh
+++ 2i-web-rerun.sh
@@ -2,4 +2,8 @@
 
 hyper config --default-region eu-central-1 --accesskey $HYPER_ACCESS --secretkey $HYPER_SECRET
 hyper pull 2innovate/2i-web
+hyper stop 2i-web
+hyper rm --volumes 2i-web
+hyper run -d -v 2i-web-keys:/root/.caddy -p 80:80 -p 443:443 --size=s1 --restart always --noauto-volume --name 2i-web 2innovate/2i-web -agree
+hyper fip attach 169.197.100.129 2i-web
 
