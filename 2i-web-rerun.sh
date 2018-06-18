595a948f54ade6a6fd209f27217595a67f1a4cf9
wondratsch <wondratsch@users.noreply.github.com>
2018-06-18 09:51:27 +0200

    add hyper pull debug

--- 2i-web-rerun.sh
+++ 2i-web-rerun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 hyper config --default-region eu-central-1 --accesskey $HYPER_ACCESS --secretkey $HYPER_SECRET
-hyper pull 2innovate/2i-web
+hyper -D pull 2innovate/2i-web
 hyper stop 2i-web
 hyper rm --volumes 2i-web
 hyper run -d -v 2i-web-keys:/root/.caddy -p 80:80 -p 443:443 --size=s1 --restart always --noauto-volume -e ACME_AGREE=true --name 2i-web 2innovate/2i-web
