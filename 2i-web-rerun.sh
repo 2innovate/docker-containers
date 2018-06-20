ccb3d1e9b897462caaf8d8b0e0ff2a77c2352e22
Gerhard Poul <gerhard.poul@gmail.com>
2018-06-20 14:53:35 +0200

    Squashed commit of the following:
    commit fb2bf8e1805761c7ef00e597dca9c3571e796f0b
    Merge: 95137ab 3526dce
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Tue Jun 19 08:19:07 2018 +0200
    
        Merge pull request #3 from wondratsch/master
    
        create/start -> run
    
    commit 3526dcefc2e5a5850c9867255fa92ad3ae1fb537
    Author: Wondratsch <wondratsch@users.noreply.github.com>
    Date:   Tue Jun 19 08:17:17 2018 +0200
    
        create/start -> run
    
    commit 95137ab20f219a0264a5e73bb46032a868de56b7
    Merge: fbf51ba 5b3bdfa
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 15:05:45 2018 +0200
    
        Merge pull request #2 from wondratsch/master
    
        fix build
    
    commit 5b3bdfac96c3e530b36e96d6afe581fc322e93ae
    Author: Wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 15:04:31 2018 +0200
    
        fix build
    
    commit c9357cfb52ce3443cf66382f6a2b50eb3f0eeb79
    Merge: 38bf1ac fbf51ba
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 13:33:13 2018 +0200
    
        Merge pull request #1 from wondratsch/patch-3
    
        Patch 3
    
    commit fbf51bab0105918c34bad402e6683a9435ccfd64
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 13:08:50 2018 +0200
    
        -pull debug
    
    commit 5f0048e792ee79e9871c47d1e6a33a24f922f3b4
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 13:07:30 2018 +0200
    
        fix snapshot bug; add fip detach
    
    commit 8b0c644f9bcad1690ac9fb8b38a9e953d87f8799
    Author: wondratsch <wondratsch@users.noreply.github.com>
    Date:   Mon Jun 18 12:24:37 2018 +0200
    
        add schwub/blue updates

--- 2i-web-rerun.sh
+++ 2i-web-rerun.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 
 hyper config --default-region eu-central-1 --accesskey $HYPER_ACCESS --secretkey $HYPER_SECRET
-hyper -D pull 2innovate/2i-web
-hyper stop 2i-web
-hyper rm --volumes 2i-web
-hyper run -d -v 2i-web-keys:/root/.caddy -p 80:80 -p 443:443 --size=s1 --restart always --noauto-volume -e ACME_AGREE=true --name 2i-web 2innovate/2i-web
-hyper fip attach 169.197.100.129 2i-web
-hyper rmi $(hyper images -f "dangling=true" -q)
+
+IMAGE="2innovate/2i-web"
+CONTAINER_NAME="2i-web"
+VOLUME_NAME="2i-web-keys"
+URL="https://staging-web.2i.at/"
+
+if [ "$(hyper volume ls -f image=$IMAGE -q)" == $VOLUME_NAME"-schwub" ]; then
+  NEW_SUFFIX="blue"
+  OLD_SUFFIX="schwub"
+else
+  NEW_SUFFIX="schwub"
+  OLD_SUFFIX="blue"
+fi
+
+hyper pull $IMAGE
+
+hyper snapshot create --name $VOLUME_NAME -v "$VOLUME_NAME"-"$OLD_SUFFIX" -f
+hyper volume create --name "$VOLUME_NAME"-"$NEW_SUFFIX" --snapshot $VOLUME_NAME
+hyper snapshot rm $VOLUME_NAME
+
+hyper run -d -v "$VOLUME_NAME"-"$NEW_SUFFIX":/root/.caddy -p 80:80 -p 443:443 --size=s1 --restart always --noauto-volume -e ACME_AGREE=true --name "$CONTAINER_NAME"-"$NEW_SUFFIX" $IMAGE
+
+sleep 60
+
+hyper fip detach "$CONTAINER_NAME"-"$OLD_SUFFIX"
+hyper fip attach 169.197.100.129 "$CONTAINER_NAME"-"$NEW_SUFFIX"
+
+hyper stop "$CONTAINER_NAME"-"$OLD_SUFFIX"
+hyper rm --volumes "$CONTAINER_NAME"-"$OLD_SUFFIX"
+hyper volume rm "$VOLUME_NAME"-"$OLD_SUFFIX"
+
+
+if [ "$(hyper images -f "dangling=true" -q)" != "" ]; then
+    hyper rmi $(hyper images -f "dangling=true" -q)
+fi
